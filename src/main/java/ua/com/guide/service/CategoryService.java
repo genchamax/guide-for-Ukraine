7266018ac69dea9653baf4d7be2342040d3096e6
Max <Max>
2016-08-09 19:40:08 +0300

    Add category service

--- src/main/java/ua/com/guide/service/CategoryService.java
+++ src/main/java/ua/com/guide/service/CategoryService.java
@@ -1,7 +1,15 @@
 package ua.com.guide.service;
 
+import org.springframework.stereotype.Service;
+import ua.com.guide.model.Category;
+
 /**
  * Created by Max on 09.08.2016.
  */
-public class CategoryService {
+@Service
+public class CategoryService extends BasicService {
+
+    public CategoryService() {
+        super(Category.class);
+    }
 }
