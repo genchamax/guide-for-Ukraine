88612cf9bdd9aa351a75e2a908d97fc0fd806e2c
Max <Max>
2016-08-10 23:31:41 +0300

    Add post rest controller

---
+++ src/main/java/ua/com/guide/controller/rest/PostRestController.java
@@ -0,0 +1,52 @@
+package ua.com.guide.controller.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ua.com.guide.model.Post;
+import ua.com.guide.service.PostService;
+
+import java.util.List;
+
+/**
+ * Created by Max on 10.08.2016.
+ */
+@RestController
+@RequestMapping("/posts")
+public class PostRestController {
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
+    public List<Post> getAllPosts() {
+        return postService.getAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Post getPostById(@PathVariable("id") Integer postId) {
+        return (Post) postService.getById(postId);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deletePostById(@PathVariable("id") Integer id) {
+        postService.deleteById(id);
+        return "redirect:";
+    }
+
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.POST)
+    public String addPost(@RequestBody Post post) {
+        postService.create(post);
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public String changePost(@PathVariable("id") Integer id, @RequestBody Post post) {
+        post.setPostId(id);
+        postService.update(post);
+        return "redirect:";
+    }
+
+
+
+}
+
