f89638ba91a2edd713c067820213d22fa0f5cf19
Max <Max>
2016-08-23 00:04:47 +0300

    Create new branch. Path start with Guide folder

--- src/main/java/ua/com/guide/service/ImageService.java
+++ src/main/java/ua/com/guide/service/ImageService.java
@@ -2,17 +2,21 @@ package ua.com.guide.service;
 
 import ua.com.guide.model.Image;
 
+import java.io.File;
+
 /**
  * Created by Max on 10.08.2016.
  */
 public class ImageService extends BasicService {
+
+    private static final String IMAGE_PATH = "src/main/webapp/static/images/";
 
     public ImageService() {
         super(Image.class);
     }
 
     public void saveImage(String regionName, String cityName, String placeName, String postId ){
-
+        System.out.println(new File(IMAGE_PATH).getAbsolutePath());
     }
 
 }
