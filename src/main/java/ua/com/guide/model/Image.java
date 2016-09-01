c34cd4655e0cf7cb288e0a12035c4f647bf81e3f
Max <Max>
2016-09-02 01:19:38 +0300

    Add config for slf4j

---
+++ src/main/java/ua/com/guide/model/Image.java
@@ -0,0 +1,46 @@
+package ua.com.guide.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by Max on 31.08.2016.
+ */
+public class ImageInfo {
+
+    private Integer regionId;
+    private Integer cityId;
+    private Integer placeId;
+    private Integer postId;
+
+    public Integer getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(Integer regionId) {
+        this.regionId = regionId;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public Integer getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(Integer placeId) {
+        this.placeId = placeId;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+}
